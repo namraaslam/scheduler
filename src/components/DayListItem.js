ebcdb2a86280b381e257aa0604e1fb130f6a1d8e
namraaslam <namra.aslam@ucalgary.ca>
2023-03-27 23:02:43 -0600

    add DayListItem dynamic styling, implement formatSpots function for test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,29 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+  };
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
